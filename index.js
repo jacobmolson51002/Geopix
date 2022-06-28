65664d48fb622f9702e442b67c5dbd11df92e761
jacobmolson51002 <jacob@promotition>
2022-06-28 06:13:34 -0500

    added login and credential storage capabilities, haven't built login yet

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import 'expo-dev-client';
 import 'react-native-get-random-values';
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { View } from 'react-native';
 import {registerRootComponent} from 'expo'
 import {AppWrapperNonSync} from './app/AppWrapperNonSync';
 import {AppWrapperSync} from './app/AppWrapperSync';
@@ -11,17 +12,55 @@ import { AppContainer } from './AppContainer';
 //import {View, Text} from 'react-native';
 import {Provider} from 'react-redux';
 import {Store} from './redux/store';
-//import {MapView} from 'react-native-maps';
+import { Login } from './views/Login';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SplashScreen from 'expo-splash-screen';
 
-const App = () =>
-  /*SYNC_CONFIG.enabled ? (
-    <AppWrapperSync appId={SYNC_CONFIG.appId} />
-  ) : (
-    <AppWrapperNonSync />
-  );*/
-  (
-  <Provider store={Store}>
-    <AppContainer />
-  </Provider>
-  );
+const App = () => {
+
+  const [appIsReady, setAppIsReady] = useState(false);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+
+        const loginCredentials = await AsyncStorage.getItem('loggedInUser');
+
+        if(loginCredentials !== null){
+          console.log('working');
+          setUserLoggedIn(true);
+        }else{
+          console.log("user not found");
+        }
+      } catch(e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+  });
+
+
+  const onLayoutRootView = useCallback(async () => {
+    if(appIsReady){
+      console.log("splash screen ready");
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  return (
+      <Provider store={Store}>
+        <View onLayout={onLayoutRootView} style={{ flex: 1 }} > 
+          {userLoggedIn ? (
+            <AppContainer />
+          ) : (
+            <Login />
+          )}
+        </View>
+      </Provider>
+  )
+}
 registerRootComponent(App);
